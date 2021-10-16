75d7f45c428140489e8e5efa88003758516e1d96
wolodymyrnosenko <wolodymyrnosenko@gmail.com>
2021-10-16 23:28:35 +0300

    Add searchField

---
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Store.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly Book[] books = new[]
+        {
+            new Book(1, "Art of Programming"),
+            new Book(2, "Refactoring"),
+            new Book(3, "C Programming Language"),
+        };
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            return books.Where(book => book.Title.Contains(titlePart))
+                        .ToArray();
+        }
+    }
+}
